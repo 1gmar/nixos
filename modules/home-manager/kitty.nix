5a6da8bb9a7615752deb41ecbea6d31af9b87f9a
Igor Marta <mr.igor.marta@gmail.com>
2025-04-06 17:19:15 +0300

    Add keybindings to kitty.

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -10,8 +10,20 @@
     programs.kitty = {
       enable = true;
       font = {
-        name = lib.mkForce "MesloLGS NF";
-        size = lib.mkForce 14;
+        name = "MesloLGS NF";
+        size = 14;
+      };
+      keybindings = {
+        "ctrl+alt+d" = "scroll_page_down";
+        "ctrl+alt+u" = "scroll_page_up";
+        "ctrl+shift+d>ctrl+shift+d" = "scroll_home";
+        "ctrl+shift+d>ctrl+shift+f" = "scroll_end";
+      };
+      settings = {
+        enabled_layouts = "splits:split_axis=auto";
+        scrollback_lines = 10000;
+        tab_bar_style = "powerline";
+        tab_powerline_style = "angled";
       };
       themeFile = "Solarized_Light";
     };
