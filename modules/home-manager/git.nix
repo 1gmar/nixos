ec0591f79a1ab3003bf5b0a31d39a6a5c9c916f9
Igor Marta <mr.igor.marta@gmail.com>
2025-07-15 18:24:45 +0300

    Add custom colors to git blame. Update Nixvim.

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -12,17 +12,19 @@
         background = "light";
         color = "auto";
         display = "inline";
-        enable = true;
+        enable = false;
         enableAsDifftool = true;
       };
       enable = true;
       userEmail = "mr.igor.marta@gmail.com";
       userName = "Igor Marta";
       extraConfig = {
+        color.blame.highlightRecent = "#268bd2,12 month ago,#2aa198,6 month ago,#859900,3 month ago,#b58900,1 month ago,#cb4b16,1 week ago,#dc322f";
         credential = {
           helper = "store";
           "https://github.com".username = "1gmar";
         };
+        difftool.prompt = false;
         init.defaultBranch = "main";
       };
     };
