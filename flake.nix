283e11a020078b0feae712d48fb1930c4014ae6c
Igor Marta <mr.igor.marta@gmail.com>
2026-01-02 17:28:34 +0200

    Switch to the stable release pipeline

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.11";
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flatpaks.url = "github:in-a-dil-emma/declarative-flatpak/latest";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager?ref=release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim-1gmar = {
@@ -18,52 +18,72 @@
     };
   };
 
-  outputs = {nixpkgs, ...} @ inputs: let
-    colors = {
-      background = "#fdf6e3";
-      backgroundHigh = "#eee8d5";
-      blue = "#268bd2";
-      cyan = "#2aa198";
-      foreground0 = "#839496";
-      foregroundEmph = "#586e75";
-      green = "#859900";
-      magenta = "#d33682";
-      orange = "#cb4b16";
-      red = "#dc322f";
-      secondaryContent = "#93a1a1";
-      text = "#657b83";
-      violet = "#6c71c4";
-      yellow = "#b58900";
-    };
-    shell-theme = ./modules/home-manager/nushell/solarized-light.nu;
-    sys-diff = ./modules/home-manager/nushell/system-diff.nu;
-    system = "x86_64-linux";
-    userName = "igmar";
-    wallpaperPath = ./anime-sky.png;
-  in {
-    nixosConfigurations = {
-      default = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit inputs system userName wallpaperPath;};
-        modules = [
-          ./hosts/default/configuration.nix
-          inputs.disko.nixosModules.default
-          ./hosts/default/disk-config.nix
-          ./modules/nixos
-        ];
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      colors = {
+        background = "#fdf6e3";
+        backgroundHigh = "#eee8d5";
+        blue = "#268bd2";
+        cyan = "#2aa198";
+        foreground0 = "#839496";
+        foregroundEmph = "#586e75";
+        green = "#859900";
+        magenta = "#d33682";
+        orange = "#cb4b16";
+        red = "#dc322f";
+        secondaryContent = "#93a1a1";
+        text = "#657b83";
+        violet = "#6c71c4";
+        yellow = "#b58900";
       };
-      desktop = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = {inherit colors inputs shell-theme sys-diff system userName wallpaperPath;};
-        modules = [
-          inputs.disko.nixosModules.default
-          ./hosts/desktop/disk-config.nix
-          ./hosts/desktop/configuration.nix
-          ./hosts/desktop/home-configuration.nix
-          ./modules/nixos
-        ];
+      shell-theme = ./modules/home-manager/nushell/solarized-light.nu;
+      sys-diff = ./modules/home-manager/nushell/system-diff.nu;
+      system = "x86_64-linux";
+      userName = "igmar";
+      wallpaperPath = ./anime-sky.png;
+    in
+    {
+      nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit
+              inputs
+              system
+              userName
+              wallpaperPath
+              ;
+          };
+          modules = [
+            ./hosts/default/configuration.nix
+            inputs.disko.nixosModules.default
+            ./hosts/default/disk-config.nix
+            ./modules/nixos
+          ];
+        };
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit
+              colors
+              inputs
+              shell-theme
+              sys-diff
+              system
+              userName
+              wallpaperPath
+              ;
+          };
+          modules = [
+            inputs.disko.nixosModules.default
+            ./hosts/desktop/disk-config.nix
+            ./hosts/desktop/configuration.nix
+            ./hosts/desktop/home-configuration.nix
+            ./modules/nixos
+          ];
+        };
       };
+      homeManagerModules.default = ./modules/home-manager;
     };
-    homeManagerModules.default = ./modules/home-manager;
-  };
 }
