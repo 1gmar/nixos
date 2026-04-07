a51fc6b6839fd1c670dd65e17e8a70ec26f5535d
Igor Marta <mr.igor.marta@gmail.com>
2026-04-07 17:39:39 +0300

    Declare the browser extensions as policies for firefox

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -1,4 +1,19 @@
-{ lib, config, ... }:
+{ config, lib, ... }:
+let
+  activity-watch = "{ef87d84c-2127-493f-b952-5b4e744245bc}";
+  better-ttv = "firefox@betterttv.net";
+  ddg-privacy = "jid1-ZAdIEUB7XOzOJw@jetpack";
+  feedbro = "{a9c2ad37-e940-4892-8dce-cd73c6cbbc0c}";
+  franker-facez = "frankerfacez@frankerfacez.com";
+  keepassxc = "keepassxc-browser@keepassxc.org";
+  proton-vpn = "vpn@proton.ch";
+  return-youtube-dislike = "{762f9885-5a13-4abd-9c77-433dcd38b8fd}";
+  rikaichamp = "{59812185-ea92-4cca-8ab7-cfcacee81281}";
+  stylus = "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}";
+  ublock = "uBlock0@raymondhill.net";
+  unhook = "myallychou@gmail.com";
+  vimium = "{d7742d87-e61d-4b78-b8a1-b469842139fa}";
+in
 {
   options.firefox = {
     enable = lib.mkEnableOption "enable firefox module";
@@ -21,6 +36,86 @@
         DisablePocket = true;
         DisableRemoteImprovements = true;
         DisableTelemetry = true;
+        ExtensionSettings = {
+          "*" = {
+            allowed_types = [
+              "theme"
+              "dictionary"
+              "locale"
+              "sitepermission"
+            ];
+            default_area = "menupanel";
+            installation_mode = "allowed";
+            private_browsing = false;
+          };
+          ${activity-watch} = lib.mkIf config.activity-watch.enable {
+            default_area = "navbar";
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${activity-watch}/latest.xpi";
+          };
+          ${better-ttv} = {
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${better-ttv}/latest.xpi";
+          };
+          ${ddg-privacy} = {
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${ddg-privacy}/latest.xpi";
+            private_browsing = true;
+          };
+          ${feedbro} = {
+            default_area = "navbar";
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${feedbro}/latest.xpi";
+          };
+          ${franker-facez} = {
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${franker-facez}/latest.xpi";
+          };
+          ${keepassxc} = lib.mkIf config.keepassxc.enable {
+            default_area = "navbar";
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${keepassxc}/latest.xpi";
+            private_browsing = true;
+          };
+          ${proton-vpn} = {
+            default_area = "navbar";
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${proton-vpn}/latest.xpi";
+            private_browsing = true;
+          };
+          ${return-youtube-dislike} = {
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${return-youtube-dislike}/latest.xpi";
+            private_browsing = true;
+          };
+          ${rikaichamp} = {
+            default_area = "navbar";
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${rikaichamp}/latest.xpi";
+            private_browsing = true;
+          };
+          ${stylus} = {
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${stylus}/latest.xpi";
+            private_browsing = true;
+          };
+          ${ublock} = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${ublock}/latest.xpi";
+            private_browsing = true;
+          };
+          ${unhook} = {
+            installation_mode = "normal_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${unhook}/latest.xpi";
+            private_browsing = true;
+          };
+          ${vimium} = {
+            default_area = "navbar";
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${vimium}/latest.xpi";
+            private_browsing = true;
+          };
+        };
         GenerativeAI = {
           Enabled = false;
           Chatbot = false;
@@ -38,10 +133,7 @@
         isDefault = true;
         settings = {
           "browser.ai.control.default" = "blocked";
-          "browser.ai.control.linkPreviewKeyPoints" = "blocked";
           "browser.ai.control.pdfjsAltText" = "blocked";
-          "browser.ai.control.sidebarChatbot" = "blocked";
-          "browser.ai.control.smartTabGroups" = "blocked";
           "browser.ai.control.translations" = "blocked";
           "browser.bookmarks.restore_default_bookmarks" = false;
           "browser.fullscreen.autohide" = false;
